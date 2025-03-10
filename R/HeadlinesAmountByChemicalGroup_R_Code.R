71b9bf5799e77ea23c65c660fd113ba095e197b2
MarcEpsteinMyExposome <marc.epstein@MyExposome.com>
2025-03-10 15:23:56 -0400

    delete old comments and restyle

--- R/HeadlinesAmountByChemicalGroup_R_Code.R
+++ R/HeadlinesAmountByChemicalGroup_R_Code.R
@@ -9,7 +9,7 @@
 #
 #     ### THESE ARE THE RULES from Silient spring BUT i slightly reworded AND skipped a few just to get something done.
 generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, debug = FALSE, debug2 = FALSE) {
-  #sampleNumber<-subject
+  # sampleNumber<-subject
   # Helper function to calculate specified percentile of a numeric vector
   calculate_percentile <- function(x, percentile) {
     quantile(x, probs = percentile / 100, na.rm = TRUE)
@@ -27,19 +27,16 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
     image = character(),
     explanation = character(),
     strategies = character(),
-    countConcern = integer(),  # count of compounds in classification for this sample that are chemicals-of-concern
+    countConcern = integer(), # count of compounds in classification for this sample that are chemicals-of-concern
     stringsAsFactors = FALSE
   )
 
   # Iterate through each classification to generate report messages
   for (class in classifications) {
-    #class <- "Pollutants from burning fuels, soot, smoke"
-
+    # class <- "Pollutants from burning fuels, soot, smoke"
 
     # get the rows ONLY that match this classification
     individual_class_data <- individual_data %>% filter(classification == class)
-
-
 
     if (debug) {
       cat("in classification loop, classification = ", class, "\n")
@@ -50,15 +47,13 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
 
     # how many chemicals of concern did this subject have for this classification
     countChemsOfConcern <- oneResult %>%
-      left_join(class_L, by="ParameterID") %>%
-      filter(classification==class) %>%
-      pull(ParameterName)%>%
+      left_join(class_L, by = "ParameterID") %>%
+      filter(classification == class) %>%
+      pull(ParameterName) %>%
       length()
 
     # # Get the list of unique samples in the dataset
     sample_count <- length(unique(testResults.bigWithClass$SampleNumber))
-
-
 
     # Initialize empty vectors for various criteria
     compounds_meeting_criteria1 <- c()
@@ -72,7 +67,7 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
 
     # Loop through each compound in the classification if it is in this individual data or NOT
     for (compound in unique(class_data$ParameterName)) {
-      #compound <- "Naphthalene" # this is one in FUELS and burning
+      # compound <- "Naphthalene" # this is one in FUELS and burning
       if (debug) {
         cat("in compound loop, compound = ", compound, "  class = ", class, "\n")
       }
@@ -97,59 +92,42 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
       pct_25 <- calculate_percentile(compound_data_non_zero$Result, 25)
       median_result <- median(compound_data_non_zero$Result, na.rm = TRUE)
 
-      #as a reminder for myself i now have lots of variables:
+      # as a reminder for myself i now have lots of variables:
       #         individual_data
       #         individual_class_data
       #         individual_class_compound_data
       #         compound_data
       #         compound_data_non_zero
 
-
-      # # Criteria 1: Result >= 95th percentile and found in <= 10% of samples
-      # if (any(individual_data$Result > 0 &
-      #         individual_data$ParameterName == compound &
-      #         individual_data$Result >= pct_95) &&
-      #     (length(compound_data_non_zero$Result) / sample_count) <= 0.10) {
-      #   compounds_meeting_criteria1 <- c(compounds_meeting_criteria1, compound)
-      # }
       # Criteria 1: Result >= 95th percentile and found in <= 10% of samples
       if (individual_class_compound_data$Result >= pct_95 &&
-          (length(compound_data_non_zero$Result) / sample_count) <= 0.10) {
+        (length(compound_data_non_zero$Result) / sample_count) <= 0.10) {
         compounds_meeting_criteria1 <- c(compounds_meeting_criteria1, compound)
       }
 
-      # # Criteria 2: Result >= 95th percentile and > 10 times the median
-      # if (any(
-      #   individual_data$Result > 0 &
-      #   individual_data$ParameterName == compound &
-      #   individual_data$Result >= pct_95 &
-      #   individual_data$Result >= (10 * median_result)
-      # )) {
-      #   compounds_meeting_criteria2 <- c(compounds_meeting_criteria2, compound)
-      # }
       # Criteria 2: Result >= 95th percentile and > 10 times the median
       if (
-        individual_class_compound_data$Result  >= pct_95 &
-        individual_class_compound_data$Result >= 10 * median_result
+        individual_class_compound_data$Result >= pct_95 &
+          individual_class_compound_data$Result >= 10 * median_result
       ) {
         compounds_meeting_criteria2 <- c(compounds_meeting_criteria2, compound)
       }
 
       # Criteria 3: Result >= 95th percentile and found in >= 10% of samples
-      if (individual_class_compound_data$Result >= pct_95   &&
+      if (individual_class_compound_data$Result >= pct_95 &&
         (length(compound_data_non_zero$Result) / sample_count) >= 0.10) {
         compounds_meeting_criteria3 <- c(compounds_meeting_criteria3, compound)
       }
       # Criteria 4: Result >= 75th percentile and >10 times median
       if (
-        individual_class_compound_data$Result  >= pct_75 &
-        individual_class_compound_data$Result > 10 * median_result
+        individual_class_compound_data$Result >= pct_75 &
+          individual_class_compound_data$Result > 10 * median_result
       ) {
         compounds_meeting_criteria4 <- c(compounds_meeting_criteria4, compound)
       }
       # Criteria 5: Result >= 75th percentile and at least 25% of samples detect
-      if (individual_class_compound_data$Result  >= pct_75 &
-          (length(compound_data_non_zero$Result) / sample_count) >= 0.25) {
+      if (individual_class_compound_data$Result >= pct_75 &
+        (length(compound_data_non_zero$Result) / sample_count) >= 0.25) {
         compounds_meeting_criteria5 <- c(compounds_meeting_criteria5, compound)
       }
       # Criteria 6: Not detected for all compounds in a group
@@ -166,23 +144,24 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
       }
       # Criteria 8: Not detected and ≤20% of measurements are non-detects
       if (individual_class_compound_data$Result == 0 &&
-          (sum(compound_data$Result == 0) / sample_count) <= 0.20) {
+        (sum(compound_data$Result == 0) / sample_count) <= 0.20) {
         compounds_meeting_criteria8 <- c(compounds_meeting_criteria8, compound)
       }
     } # end compound loop
 
     if (debug2) {
-      cat("Criteria for classification:", class, "\n",
-          "crit1=", compounds_meeting_criteria1, "\n",
-          "crit2=", compounds_meeting_criteria2, "\n",
-          "crit3=", compounds_meeting_criteria3, "\n",
-          "crit4=", compounds_meeting_criteria4, "\n",
-          "crit5=", compounds_meeting_criteria5, "\n",
-          "crit6=", compounds_meeting_criteria6, "\n",
-          "crit7=", compounds_meeting_criteria7, "\n",
-          "crit8=", compounds_meeting_criteria8, "\n")
+      cat(
+        "Criteria for classification:", class, "\n",
+        "crit1=", compounds_meeting_criteria1, "\n",
+        "crit2=", compounds_meeting_criteria2, "\n",
+        "crit3=", compounds_meeting_criteria3, "\n",
+        "crit4=", compounds_meeting_criteria4, "\n",
+        "crit5=", compounds_meeting_criteria5, "\n",
+        "crit6=", compounds_meeting_criteria6, "\n",
+        "crit7=", compounds_meeting_criteria7, "\n",
+        "crit8=", compounds_meeting_criteria8, "\n"
+      )
     }
-
 
     # Generate a sanitized modal id using  existing function
     #  THIS SAME modal id is used generating the BS MODAL dialog in an "html" section in the top of the main Rmd file
@@ -198,20 +177,17 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
         "<strong>",
         class,
         "</strong></a>",
-
         " chemicals detected at a high level that were not found in most other wristbands."
       )
     } else if (length(compounds_meeting_criteria1) == 1) {
       message <- paste0( #               THIS IS WHAT I"M MESSING WITH NOW THIS IS WHAT I"M MESSING WITH NOW THIS IS WHAT I"M MESSING WITH NOW THIS IS WHAT I"M MESSING WITH NOW
         "You had an ",
-
         "<a href='#' data-toggle='modal' data-target='#",
         modal_id,
         "'>",
         "<strong>",
         class,
         "</strong></a>",
-
         " compound, ",
         generateTabLink(compounds_meeting_criteria1[1]),
         " detected at a high level that was not found in most other wristbands."
@@ -219,27 +195,23 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
     } else if (length(compounds_meeting_criteria2) > 1) {
       message <- paste0(
         "You had higher levels of ",
-
         "<a href='#' data-toggle='modal' data-target='#",
         modal_id,
         "'>",
         "<strong>",
         class,
         "</strong></a>",
-
         " compared to 95% of the other people."
       )
     } else if (length(compounds_meeting_criteria2) == 1) {
       message <- paste0(
         "You had a higher level of ",
-
         "<a href='#' data-toggle='modal' data-target='#",
         modal_id,
         "'>",
         "<strong>",
         class,
         "</strong></a>",
-
         " compound, ",
         generateTabLink(compounds_meeting_criteria2[1]),
         ", compared to 95% of other people in the study."
@@ -247,79 +219,67 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
     } else if (length(compounds_meeting_criteria3) > 0) {
       message <- paste0(
         "You had higher levels of ",
-
         "<a href='#' data-toggle='modal' data-target='#",
         modal_id,
         "'>",
         "<strong>",
         class,
         "</strong></a>",
-
         " compared to 95% of the other people."
       )
     } else if (length(compounds_meeting_criteria4) > 0) {
       message <- paste0(
         "You had higher levels of ",
-
         "<a href='#' data-toggle='modal' data-target='#",
         modal_id,
         "'>",
         "<strong>",
         class,
         "</strong></a>",
-
         " compared to 75% of the other people. Some of your numbers are much higher than most others."
       )
     } else if (length(compounds_meeting_criteria5) > 0) {
       message <- paste0(
         "You had higher levels of ",
-
         "<a href='#' data-toggle='modal' data-target='#",
         modal_id,
         "'>",
         "<strong>",
         class,
         "</strong></a>",
-
         " compared to 75% of the other people."
       )
     } else if (length(compounds_meeting_criteria6) == 0) {
       message <- paste0(
         "You had no ",
-
         "<a href='#' data-toggle='modal' data-target='#",
         modal_id,
         "'>",
         "<strong>",
         class,
         "</strong></a>",
-
         " detected in your wristband."
       )
-    } else if (length(compounds_meeting_criteria7) == 0) {  # We only added compounds to this list if they were GREATER than 50% so if we have any then we don't meet criteria 7
+    } else if (length(compounds_meeting_criteria7) == 0) { # We only added compounds to this list if they were GREATER than 50% so if we have any then we don't meet criteria 7
       message <- paste0(
         "You had lower levels of ",
-
         "<a href='#' data-toggle='modal' data-target='#",
         modal_id,
         "'>",
         "<strong>",
         class,
         "</strong></a>",
-
         " than most others."
       )
     } else if (length(compounds_meeting_criteria8) > 0) {
       message <- paste0(
         "You did not have some ",
-
         "<a href='#' data-toggle='modal' data-target='#",
         modal_id,
         "'>",
         "<strong>",
         class,
         "</strong></a>",
-
         " detected in your wristband that were found in most others."
       )
     }
@@ -328,10 +288,10 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
     # Lookup additional info from the classExplainTable tibble
     match_row <- classExplainTable %>% filter(`Chemical Group` == class)
 
-    #classExplainTable$`Chemical Group`[6]
-    #class
+    # classExplainTable$`Chemical Group`[6]
+    # class
 
-    if(nrow(match_row) > 0) {
+    if (nrow(match_row) > 0) {
       image_file <- match_row$ImageFile[1]
       explanation_text <- match_row$Description[1]
       strategies_text <- match_row$Strategies[1]
@@ -348,7 +308,7 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
       image = image_file,
       explanation = explanation_text,
       strategies = strategies_text,
-      countConcern= countChemsOfConcern,
+      countConcern = countChemsOfConcern,
       stringsAsFactors = FALSE
     )
     report_table <- rbind(report_table, new_row)
@@ -362,7 +322,7 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
 }
 
 
-#generate_report2(sampleNumber, testResults.bigWithClass, debug = FALSE, debug2 = FALSE)
+# generate_report2(sampleNumber, testResults.bigWithClass, debug = FALSE, debug2 = FALSE)
 
 
 # Print the messages
