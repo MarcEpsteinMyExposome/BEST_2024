0686fc8db2c5d88e9a58b2447746992f67384e81
MarcEpsteinMyExposome <marc.epstein@MyExposome.com>
2025-03-11 11:29:21 -0400

    use claude / chatgpt to improve error handling and debugging in function and make some code clearer and cleaner.  Added standard doc to top of function.

--- R/HeadlinesAmountByChemicalGroup_R_Code.R
+++ R/HeadlinesAmountByChemicalGroup_R_Code.R
@@ -3,17 +3,115 @@
 # The report is based on percentile criteria and compares the sample's results to
 # those of a broader dataset.
 
+#
+# HERE ARE SAMPLES OF THE 3 variables being passed into function:
+#   > str(subject)
+# chr "A241576"
+# > str(testResults.bigWithClass)
+# tibble [15,158 × 5] (S3: tbl_df/tbl/data.frame)
+# $ ParameterID   : chr [1:15158] "1023" "1023" "1023" "1023" ...
+# $ ParameterName : chr [1:15158] "Tri-p-tolyl phosphate" "Tri-p-tolyl phosphate" "Tri-p-tolyl phosphate" "Tri-p-tolyl phosphate" ...
+# $ SampleNumber  : chr [1:15158] "A241133" "A241134" "A241137" "A241139" ...
+# $ Result        : num [1:15158] 0 0 0 0 0 0 0 0 0 0 ...
+# $ classification: chr [1:15158] "Industrial & Commercial Chemicals" "Industrial & Commercial Chemicals" "Industrial & Commercial Chemicals" "Industrial & Commercial Chemicals" ...
+# > str (oneResult)
+# tibble [20 × 26] (S3: tbl_df/tbl/data.frame)
+# $ ParameterID           : chr [1:20] "69" "303000" "301134" "303324" ...
+# $ SampleNumber          : chr [1:20] "A241576" "A241576" "A241576" "A241576" ...
+# $ Days_worn             : num [1:20] 1.25 1.25 1.25 1.25 1.25 1.25 1.25 1.25 1.25 1.25 ...
+# $ Result                : num [1:20] 1.08 21.7 28.9 37.9 253 560 155 325 470 3250 ...
+# $ ResultOriginal        : num [1:20] 6 120 160 210 1400 3100 860 1800 2600 18000 ...
+# $ Flag                  : chr [1:20] "J" "" "" "" ...
+# $ MyE_Received          : chr [1:20] "8/21/2024" "8/21/2024" "8/21/2024" "8/21/2024" ...
+# $ size_factor           : num [1:20] 4.43 4.43 4.43 4.43 4.43 4.43 4.43 4.43 4.43 4.43 ...
+# $ week_factor           : num [1:20] 0.18 0.18 0.18 0.18 0.18 0.18 0.18 0.18 0.18 0.18 ...
+# $ Lab_Submission_Batch  : chr [1:20] "PO 262" "PO 262" "PO 262" "PO 262" ...
+# $ ParameterName         : chr [1:20] "1-Methylnaphthalene" "B-Ionone" "Benzophenone" "Benzothiazole" ...
+# $ CASNumber             : chr [1:20] "90-12-0" "79-77-6" "119-61-9" "95-16-9" ...
+# $ PureSampleName        : chr [1:20] "175-WB" "175-WB" "175-WB" "175-WB" ...
+# $ norm_Result           : num [1:20] 0.06136 0.00756 0.04412 0.05074 0.00948 ...
+# $ quartile              : num [1:20] 1 1 1 1 2 3 3 3 2 4 ...
+# $ is_this_max           : logi [1:20] FALSE FALSE FALSE FALSE FALSE FALSE ...
+# $ endocrine_toxicity    : num [1:20] 0 0 0 0 0 0 1 0 1 0 ...
+# $ respiratory_toxicity  : num [1:20] 0 0 0 0 0 0 0 0 0 0 ...
+# $ carcinogenicity       : num [1:20] 0 0 1 0 0 0 0 0 0 0 ...
+# $ genotoxicity          : num [1:20] 0 0 0 0 0 0 0 0 0 0 ...
+# $ reproductive_toxicity : num [1:20] 0 0 0 0 0 0 1 0 1 0 ...
+# $ developmental_toxicity: num [1:20] 0 0 0 0 0 0 1 0 1 0 ...
+# $ neurotoxicity         : num [1:20] 1 0 0 0 0 0 0 0 0 0 ...
+# $ pbt                   : num [1:20] 0 0 0 0 0 0 0 0 0 0 ...
+# $ calSaferURL           : chr [1:20] "https://calsafer.dtsc.ca.gov/cms/candidate-chemical/?rid=22381" NA "https://calsafer.dtsc.ca.gov/cms/candidate-chemical/?rid=20641" NA ...
+# $ tot_haz               : num [1:20] 1 0 1 0 0 0 3 0 3 0 ...
+
+
+
 
 # Here we implement the headline rules defined by silent spring
 # Define the function
 #
 #     ### THESE ARE THE RULES from Silient spring BUT i slightly reworded AND skipped a few just to get something done.
-generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, debug = FALSE, debug2 = FALSE) {
+#' Generate Chemical Classification Report
+#'
+#' This function processes chemical test results for a sample and generates
+#' a detailed report on chemical classifications detected in the sample.
+#' The report includes messages, images, explanations, and strategies for each
+#' classification based on various criteria.
+#'
+#' @param sampleNumber Character. The ID of the sample to analyze.
+#' @param testResults.bigWithClass Tibble. Dataset containing test results for all samples.
+#' @param oneResult Tibble. Dataset containing detailed results for the specific sample.
+#' @param classExplainTable Tibble. Lookup table containing descriptions, strategies, and image files for each chemical classification.
+#' @param class_L Tibble. Lookup table mapping chemical parameter IDs to their classifications.
+#' @param debug Logical. If TRUE, prints additional diagnostic information.
+#' @param debug2 Logical. If TRUE, prints detailed criteria evaluation information.
+#'
+#' @return A data frame (tibble) with the following columns:
+#'   \item{classification}{Character. The chemical classification group.}
+#'   \item{message}{Character. The personalized message for this classification.}
+#'   \item{image}{Character. Filename of the associated image.}
+#'   \item{explanation}{Character. Explanation text for this classification.}
+#'   \item{strategies}{Character. Recommended strategies for this classification.}
+#'   \item{countConcern}{Integer. Count of compounds of concern in this classification.}
+#'
+#' @examples
+#' # Generate report for sample "A241576"
+#' report <- generate_report2("A241576", testResults.bigWithClass, oneResult,
+#'                            classExplainTable, class_L)
+generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, classExplainTable, class_L , debug = FALSE, debug2 = FALSE) {
   # sampleNumber<-subject
   # Helper function to calculate specified percentile of a numeric vector
   calculate_percentile <- function(x, percentile) {
     quantile(x, probs = percentile / 100, na.rm = TRUE)
   }
+
+
+  # Add validation for input data
+  if (nrow(testResults.bigWithClass) == 0 || is.null(testResults.bigWithClass)) {
+    stop("Error: testResults.bigWithClass is empty or NULL")
+  }
+
+  if (nrow(oneResult) == 0 || is.null(oneResult)) {
+    stop("Error: oneResult is empty or NULL")
+  }
+
+  # Validate required columns exist
+  required_cols_big <- c("SampleNumber", "ParameterName", "Result", "classification")
+  required_cols_one <- c("SampleNumber", "ParameterID", "ParameterName")
+
+  if (!all(required_cols_big %in% colnames(testResults.bigWithClass))) {
+    stop("Error: testResults.bigWithClass is missing required columns")
+  }
+
+  if (!all(required_cols_one %in% colnames(oneResult))) {
+    stop("Error: oneResult is missing required columns")
+  }
+
+  # Validate that the sampleNumber exists in oneResult
+  if (!(sampleNumber %in% oneResult$SampleNumber)) {
+    stop(paste("Error: sampleNumber", sampleNumber, "not found in oneResult data"))
+  }
+
+
 
   # so individual_data will be all the data for this one user sample
   individual_data <- testResults.bigWithClass %>% filter(SampleNumber == sampleNumber)
@@ -61,8 +159,11 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
     compounds_meeting_criteria3 <- c()
     compounds_meeting_criteria4 <- c()
     compounds_meeting_criteria5 <- c()
-    compounds_meeting_criteria6 <- c()
-    compounds_meeting_criteria7 <- c()
+    # Initialize flags at the beginning of each classification loop
+    criteria6_all_compounds_not_detected = TRUE  # For criteria 6.  Originally did it by adding compound to list but that was confusing and inverted
+    criteria7_all_compounds_below_50th = TRUE    # For criteria 7 Originally did it by adding compound to list but that was confusing and inverted
+    #compounds_meeting_criteria6 <- c()
+    #compounds_meeting_criteria7 <- c()
     compounds_meeting_criteria8 <- c()
 
     # Loop through each compound in the classification if it is in this individual data or NOT
@@ -134,13 +235,13 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
       # so here what i want to do is check if a compound IS detected for this subject because then if ANY compound is we do NOT meet criteria 6
       # NOTE i could more easily just check here if   countChemsOfConcern   is equal 0...that should be same and doesn't need to be in this loop but...
       if (individual_class_compound_data$Result != 0) {
-        compounds_meeting_criteria6 <- c(compounds_meeting_criteria6, compound)
+        criteria6_all_compounds_not_detected = FALSE  # If any compound is detected, criteria 6 is not met
       }
       # Criteria 7: Result < 50th percentile for all compounds in group
       #     and we know that at least ONE compound is FOUND for this group from test 6 above
       #    so we can just check if this compound is GREATER that 50% and if it is then add it to list and that means we don't meet criteria 7
       if (individual_class_compound_data$Result > pct_50) {
-        compounds_meeting_criteria7 <- c(compounds_meeting_criteria7, compound)
+        criteria7_all_compounds_below_50th = FALSE  # If any compound is above 50th, criteria 7 is not met
       }
       # Criteria 8: Not detected and ≤20% of measurements are non-detects
       if (individual_class_compound_data$Result == 0 &&
@@ -149,19 +250,6 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
       }
     } # end compound loop
 
-    if (debug2) {
-      cat(
-        "Criteria for classification:", class, "\n",
-        "crit1=", compounds_meeting_criteria1, "\n",
-        "crit2=", compounds_meeting_criteria2, "\n",
-        "crit3=", compounds_meeting_criteria3, "\n",
-        "crit4=", compounds_meeting_criteria4, "\n",
-        "crit5=", compounds_meeting_criteria5, "\n",
-        "crit6=", compounds_meeting_criteria6, "\n",
-        "crit7=", compounds_meeting_criteria7, "\n",
-        "crit8=", compounds_meeting_criteria8, "\n"
-      )
-    }
 
     # Generate a sanitized modal id using  existing function
     #  THIS SAME modal id is used generating the BS MODAL dialog in an "html" section in the top of the main Rmd file
@@ -249,7 +337,7 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
         "</strong></a>",
         " compared to 75% of the other people."
       )
-    } else if (length(compounds_meeting_criteria6) == 0) {
+    } else if (criteria6_all_compounds_not_detected) {
       message <- paste0(
         "You had no ",
         "<a href='#' data-toggle='modal' data-target='#",
@@ -260,7 +348,7 @@ generate_report2 <- function(sampleNumber, testResults.bigWithClass, oneResult, 
         "</strong></a>",
         " detected in your wristband."
       )
-    } else if (length(compounds_meeting_criteria7) == 0) { # We only added compounds to this list if they were GREATER than 50% so if we have any then we don't meet criteria 7
+    } else if (criteria7_all_compounds_below_50th) { #
       message <- paste0(
         "You had lower levels of ",
         "<a href='#' data-toggle='modal' data-target='#",
